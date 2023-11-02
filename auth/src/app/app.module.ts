ad6c52d700d1261228a8d22523fe9d81a2910ca5
binnie <lananh4415@gmail.com>
2023-11-02 14:02:11 +0700

    push push

--- auth/src/app/app.module.ts
+++ auth/src/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
-import {NzMenuModule} from "ng-zorro-antd/menu";
-import {NzLayoutModule} from "ng-zorro-antd/layout";
-import {NzFormModule} from "ng-zorro-antd/form";
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { LockOutline, UserOutline } from '@ant-design/icons-angular/icons';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+const icons: IconDefinition[] = [UserOutline, LockOutline];
 
 @NgModule({
   declarations: [AppComponent, RegisterComponent],
@@ -25,7 +30,8 @@ import {NzFormModule} from "ng-zorro-antd/form";
     ),
     NzMenuModule,
     NzLayoutModule,
-    NzFormModule
+    NzFormModule,
+    NzIconModule.forChild(icons),
   ],
   providers: [],
   bootstrap: [AppComponent],
